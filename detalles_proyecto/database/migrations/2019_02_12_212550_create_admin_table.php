cc68f1bf44a3b3c4bfe87f73600e4d1f9c5ef7d2
Carol Sandi <caroladrisf@gmail.com>
2019-02-12 16:43:39 -0600

    Admin migration

---
+++ detalles_proyecto/database/migrations/2019_02_12_212550_create_admin_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('admin', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('nombre');
+            $table->text('apellido');
+            $table->text('correo');
+            $table->text('username');
+            $table->text('password');
+            $table->text('rol');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('admin', function (Blueprint $table) {
+            //
+        });
+    }
+}
