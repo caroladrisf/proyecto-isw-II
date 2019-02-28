63a03bf08566c5e075a081fbc333218cb8f7d723
Carol Sandi <caroladrisf@gmail.com>
2019-02-27 19:49:20 -0600

    Muevo la vista de login a la carpeta admin

--- detalles_proyecto/resources/views/admin/login.blade.php
+++ detalles_proyecto/resources/views/admin/login.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="mx-auto" style="width: 500px">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    </div>
 
     <div class="card border-light mx-auto shadow" id="login">
         <div class="card-header text-center pt-4">
@@ -10,12 +21,12 @@
             <form action="{{ action('AdminController@session') }}" method="POST">
             @csrf
                 <div class="form-group">
-                    <label>Correo electrónico o usuario</label>
-                    <input type="text" name="username" class="form-control" placeholder="Correo electrónico o usuario">
+                    <label>Usuario</label>
+                    <input type="text" name="username" class="form-control" value="{{ old('username') }}" placeholder="Usuario" required>
                 </div>
                 <div class="form-group">
                     <label>Contraseña</label>
-                    <input type="password" name="password" class="form-control" placeholder="Contraseña">
+                    <input type="password" name="password" class="form-control" placeholder="Contraseña" required>
                 </div>
                 <div class="text-center w-50 mx-auto pt-2">
                     <button type="submit" class="btn btn-primary btn-block"><strong>Ingresar</strong></button>
